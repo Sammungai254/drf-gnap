9074bade712c0c55418792ed4e75b97cc65e4ee8
samuel mungai <sam.mungaike@gmail.com>
2026-04-15 16:37:50 +0300

    Project Structuring

--- apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class DRFGNAPConfig(AppConfig):
-    name = "drf_gnap"
-    verbose_name = "DRF GNAP"
-
-    def ready(self) -> None:
-        from drf_gnap import checks  # noqa: F401 - register system checks
